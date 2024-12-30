187af36b2ff97e4a85f7e4db357b6fd86ba582d9
kevin <none>
2024-12-30 03:17:32 +0000

    add test_fastmodel_api.py

---
+++ test/test_fastmodel_api.py
@@ -0,0 +1,17 @@
+# 使用SDK前需要安装python库  终端运行：pip install fastmodels-kit
+from fastmodels import Client
+import configparser
+
+config = configparser.ConfigParser()
+config.read('../config.ini')
+
+# 初始化客户端
+client = Client(
+    api_key= config['API_KEYS']['api_key'],
+    project_id= config['API_KEYS']['project_id']
+)
+# 创建转录任务
+create_task_response = client.easyllm.speech_to_text.create(
+    easyllm_id=config['API_KEYS']['easyllm_id'],
+    audio_url="https://drive.google.com/file/d/18uvPASlQea85PGlFZURZIUHIC4FtHsXE/view?usp=sharing",
+)
