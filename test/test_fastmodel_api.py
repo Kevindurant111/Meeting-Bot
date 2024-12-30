f59e9d32e212786b144e84499f45a32ff3b79b6f
kevin <none>
2024-12-30 06:15:39 +0000

    test failed

--- test/test_fastmodel_api.py
+++ test/test_fastmodel_api.py
@@ -10,8 +10,17 @@ client = Client(
     api_key= config['API_KEYS']['api_key'],
     project_id= config['API_KEYS']['project_id']
 )
-# 创建转录任务
-create_task_response = client.easyllm.speech_to_text.create(
-    easyllm_id=config['API_KEYS']['easyllm_id'],
-    audio_url="https://drive.google.com/file/d/18uvPASlQea85PGlFZURZIUHIC4FtHsXE/view?usp=sharing",
-)
+# # 创建转录任务
+# create_task_response = client.easyllm.speech_to_text.create(
+#     easyllm_id=config['API_KEYS']['easyllm_id'],
+#     audio_url="https://drive.google.com/file/d/18uvPASlQea85PGlFZURZIUHIC4FtHsXE/view?usp=sharing",
+# )
+
+# print(create_task_response)
+# print(create_task_response.task_id)
+# task_id = create_task_response.task_id
+
+task_id = 11299279156
+task = client.easyllm.speech_to_text.get(task_id)
+print(task.status)
+print(task.result)
